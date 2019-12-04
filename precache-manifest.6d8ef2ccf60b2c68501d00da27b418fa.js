978d9ce7ceb2496b10466b4b29828100fcb8ff13
AzatErnis <azat.ernis@gmail.com>
2019-12-04 15:20:24 +0600

    Updates

---
+++ precache-manifest.6d8ef2ccf60b2c68501d00da27b418fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bc91533db88763e9da729e153b488a",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "f7aad51dfc6816c77d2e",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "22c7d26c42b7cce462e5",
+    "url": "/RoboFriends/static/css/main.3dc26948.chunk.css"
+  },
+  {
+    "revision": "f7aad51dfc6816c77d2e",
+    "url": "/RoboFriends/static/js/2.eebb5bff.chunk.js"
+  },
+  {
+    "revision": "22c7d26c42b7cce462e5",
+    "url": "/RoboFriends/static/js/main.376573ba.chunk.js"
+  },
+  {
+    "revision": "d26ee320d0e256f2c408",
+    "url": "/RoboFriends/static/js/runtime-main.e96822ad.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
